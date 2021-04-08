89adbaf9fef7126b36cd2a6b8dfb3dcfc09ddec1
thiago <thiagoprocaci@gmail.com>
2021-04-08 20:16:58 -0300

    readme!

--- src/main/java/com/tbp/Application.java
+++ src/main/java/com/tbp/Application.java
@@ -1,9 +1,15 @@
 package com.tbp;
 
+import java.util.ArrayList;
+
 public class Application {
 
     public static void main(String[] args) {
         System.out.println("abc");
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(0 , "a");
+        System.out.println(arrayList);
+     
     }
 
 }
